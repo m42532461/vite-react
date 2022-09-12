6733a5f599d446cf65e24612b8ac9d53f4269d91
m42532461 <m42532461@gmail.com>
2022-09-12 21:02:53 +0800

    persist redux for authentication

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,10 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Success from "./pages/Success";
+import { useSelector } from "react-redux";
+
 const App = () => {
-  const user = true;
+  const user = useSelector((state) => state.user.currentUser);
   return (
     <BrowserRouter>
       <Routes>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../redux/apiCalls";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    login(dispatch, { username, password });
+  };
+
   return (
     <div
       className="w-[100vw] h-[100vh] bg-cover flex justify-center items-center"
@@ -16,15 +28,25 @@ const Login = () => {
           <input
             className="flex-1 min-w-2/5 my-[10px] p-[10px]"
             placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
           />
           <input
             className="flex-1 min-w-2/5 my-[10px] p-[10px]"
             placeholder="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
           />
           <span className="text-[12px] my-[20px]"></span>
-          <button className="w-2/5 py-[15px] px-[20px] bg-teal-600 text-white cursor-pointer mb-[10px]">
+          <button
+            onClick={handleClick}
+            // disabled={isFetching}
+            className={`w-2/5 py-[15px] px-[20px] bg-teal-600 text-white cursor-pointer mb-[10px] ${
+              isFetching && "cursor-not-allowed bg-green-200"
+            }`}
+          >
             LOGIN
           </button>
+          <span className=" text-red-600">Something went wrong</span>
           <a
             href="/"
             className=" my-[5px] text-[12px] underline cursor-pointer"
