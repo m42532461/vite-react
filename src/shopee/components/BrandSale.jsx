cab25725f25a6e1b8216f6f9cd0e81a50fd7fc80
m42532461 <m42532461@gmail.com>
2022-10-06 16:49:33 +0800

    topbar

---
+++ src/shopee/components/BrandSale.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BrandSale = () => {
+  return <div>BrandSale</div>;
+};
+
+export default BrandSale;
---
+++ src/shopee/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Banner from "../components/Banner";
+import BrandSale from "../components/BrandSale";
+import Categories from "../components/Categories";
+import SpecialSale from "../components/SpecialSale";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <img src="" alt="small banner" />
+      <BrandSale />
+      <Categories />
+      <SpecialSale />
+    </div>
+  );
+};
+
+export default Home;
